cf7e94037f6f4ab81d993983b80c8c1afdf1950f
Mahdi <mybodygurd@gmail.com>
2025-05-05 19:31:58 +0330

    - Apply Black formatting to src/main.py - Add pyproject.toml , requirements.txt, .pre-commit-config.yaml files for the project - Add test folder

--- src/main.py
+++ src/main.py
@@ -5,52 +5,53 @@ from typing import Optional
 
 def list_items(dir: str) -> Optional[list[tuple[str, str]]]:
     items = []
-            
+
     if os.path.exists(dir):
 
         try:
             # if os.path.isdir(path):
-                # ...
+            # ...
             for item in os.listdir(dir):
                 item_path = os.path.join(dir, item)
-                
+
                 if os.path.isfile(item_path):
-                    items.append((item, 'file'))
+                    items.append((item, "file"))
                 else:
-                    items.append((item, 'dir'))
+                    items.append((item, "dir"))
             return items
         except PermissionError:
             raise
         except NotADirectoryError:
             raise
     return None
-                         
+
+
 def main():
     print("File Management CLI")
     while True:
         try:
-            print('=' * 100)
+            print("=" * 100)
             print("1. List Files")
             print("2. Exit")
             choice = input("Enter choice (1-2): ").strip()
-            
-            if choice == '1':
+
+            if choice == "1":
                 dir = input("Enter directory (leave blank for current): ").strip()
                 if not dir:
                     dir = os.getcwd()
-                files_name = list_items(dir)  
-                
+                files_name = list_items(dir)
+
                 if files_name is None:
                     print(f"Directory not found: {dir}")
                     continue
-                
+
                 if len(files_name) == 0:
                     print("(no output)")
                     continue
-                
+
                 for file_name, type in files_name:
                     print(file_name, f"({type})")
-            elif choice == '2':
+            elif choice == "2":
                 sys.exit("Exiting...")
             else:
                 print("Invalid choice!")
@@ -60,7 +61,7 @@ def main():
             print(f"not allowed to access directory: {dir}")
         except NotADirectoryError:
             print(f"invalid directory path: {dir}")
-            
+
 
 if __name__ == "__main__":
     main()
---
+++ tests/test_main.py
@@ -0,0 +1,8 @@
+import os
+
+import pytest
+
+from src.main import list_items
+
+
+def test_user_input(): ...
